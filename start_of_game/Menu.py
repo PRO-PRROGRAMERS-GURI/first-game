f5680bd392be38bf34238ec866541e0ad68e37cf
gurilad <giladbh21@gmail.com>
2020-04-05 11:54:00 +0300

    system and menu but unknown error soo yea fix it if you can

---
+++ start_of_game/Menu.py
@@ -0,0 +1,24 @@
+def manu():
+    def name():
+        name = input('enter your name here ')
+        if name == "Guri":
+            print('this name is alredy taken')
+            name()
+        if name == "sans":
+            print('this name is alredy taken')
+            name()
+        if name == "pip":
+            print('this name is alredy taken')
+            name()
+            
+    print("press enter on the option you want to select")
+    new = input('New Game')
+    if new == "":
+        #put here start thing
+
+
+    load = input('Load')
+    if load == "":
+        #saves here
+    manu()
+    
---
+++ systems.py
@@ -0,0 +1,20 @@
+import time
+#/\/\/\/\ for the happines system
+
+#happines
+happines = 0
+#starter money i guess if u want to change it do it
+money = 200
+if money >= 1000:
+    if time.sleep(150) == True:
+        happines = happiness + 6
+if money >= 2000:
+    if time.sleep(300) == True:
+        happines = happiness + 12
+#XP SYTEM I GUESS if u want it
+xp = 0
+#jobs
+job = '??? do what you want'
+#time system
+
+#can you do the time system idk how to do it and the job system
