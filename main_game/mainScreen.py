64a10d64c95ca296db834e36f4107bfd2929c87c
assaftr <assaf2treves2@gmail.com>
2020-04-13 00:13:04 +0300

    added new option

--- main_game/mainScreen.py
+++ main_game/mainScreen.py
@@ -4,7 +4,7 @@ time.sleep(1)
 print("our game is textbase so you need to type what you do")
 time.sleep(1)
 def mainScreen():
-    print("wellcome to mew mew city here you can go to appliance job, school, home, vet")
+    print("wellcome to mew mew city here you can go to appliance job, school, home, gym, ikea, mcdonalds")
     GoTo=input("just type where you wana go ")
     if GoTo == "appliance job":
         print("wellcome to:", GoTo)
@@ -15,9 +15,15 @@ def mainScreen():
     elif GoTo == "home":
         print("wellcome to:", GoTo)
         #home system
-    elif GoTo == "vet":
+    elif GoTo == "gym":
         print("wellcome to:", GoTo)
-        #vet system
+        #gym system
+    elif GoTo == "mcdonalds":
+        print("wellcome to:", GoTo)
+        #mcdonalds system.py
+    elif GoTo == "ikea":
+        print("wellcome to:", GoTo)
+        #ikea system.py
     else:
         print("there is no place like that here")
         time.sleep(0.5)
