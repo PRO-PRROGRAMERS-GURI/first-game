c3831f218feaa220be85362e392534f7b054259d
assaftr <assaf2treves2@gmail.com>
2020-04-12 18:52:17 +0300

    make job system smarter

--- main_game/Job.System.py
+++ main_game/Job.System.py
@@ -1,8 +1,10 @@
 intelegens = 0
 fitness = 0
+xp = 0
 def jobsystem():
     job_apply = input('want a job? ')
     if job_apply == 'yes':
+        time = time - 1
         job = input('''
         What job do you want
 
@@ -19,38 +21,38 @@ def jobsystem():
 
 
         ''')
-    if job == "gym worker":
-        if fitness == 1:
-            print('you are now a gym worker')
-        if fitness <= 1:
-            print('you do not have enogh fitness')
-        if fitness >= 1:
-            print('you are now a gym worker')
-    elif job == 'teacher':
-        if intelegens == 2:
-            print('you are now a teacher')
-        if intelegens <= 2:
-            print('go learn kid you are not smart enogh')
-        if intelegens >= 2:
-            print('you are now a teacher')
-    elif job == 'film star':
+    if job == 'film star':
         print('you are now a film star')
-    elif job == 'game develepor':
-        if intelegens == 3:
-            print('you are now a game develepor')
-        if intelegens <= 3:
+    elif job == "gym worker":
+        if fitness >= 1 and  xp >= 25:
+            print('you are now a gym worker')
+        if fitness < 1:
+            print('you do not have enogh fitness')
+        if  xp < 25:
+            print("you don't have enogh work xp")
+    elif job == "vet":
+        if xp >= 100 and intelegens >= 1:
+            print('you are now a vet')
+        if intelegens < 1:
             print('go learn kid you are not smart enogh')
-        if intelegens >= 3:
-            print('you are now a develepor')
+        if xp < 100:
+            print("you don't have enogh work xp")            
+    elif job == 'teacher':
+        if intelegens >= 2 and xp >= 200:
+            print('you are now a teacher')
+        if intelegens < 2:
+            print('go learn kid you are not smart enogh')
+        if xp < 200:
+            print("you don't have enogh work xp")
+    elif job == 'game develepor':
+        if xp >= 500 and intelegens >= 3:
+            print('you are now a game develepor')
+        if intelegens < 3:
+            print('go learn kid you are not smart enogh')
         if intelegens == 2:
             print('you cant be a game develepor but you can be a teacher')
-    elif job == "vet":
-        if intelegens == 1:
-            print('you are now a vet')
-        if intelegens <= 1:
-            print('go learn kid you are not smart enogh')
-        if intelegens >= 1:
-            print('you are now a vet')
+        if xp < 500:
+            print("you don't have enogh work xp")
     else:
         print('try agian no job like that or you used capslock it wouldnt')
         jobsystem()
